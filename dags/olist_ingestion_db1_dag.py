63716a0fd0f38df8d2125d0347d6f748f4e4ec06
ayahhany <ayahhhany@gmail.com>
2025-08-28 14:44:34 +0300

    chore() : editing merge logic

--- dags/olist_ingestion_db1_dag.py
+++ dags/olist_ingestion_db1_dag.py
@@ -3,6 +3,7 @@ from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToG
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from datetime import datetime, timedelta
+from schema_utils import get_table_columns_from_postgres, generate_merge_sql
 
 # orders_products_db
 POSTGRES_CONN_ID = "postgres_olist_db1_ayahany" 
@@ -10,13 +11,14 @@ GCS_BUCKET = "ready-labs-postgres-to-gcs"
 BIGQUERY_DATASET = "ready-de26.project_landing" 
 TABLES_DB1 = {
     "orders": "order_id",
-#     "order_items": "order_item_id",
-#     "order_reviews": "review_id",
-#     "products": "product_id",
-#     "product_category_name_translation": "product_category_name"
+    "order_items": "order_item_id",
+    "order_reviews": "review_id",
+    "products": "product_id",
+    "product_category_name_translation": "product_category_name"
 }
 TIMESTAMP_COLUMN = "updated_at_timestamp"
- 
+
+schema = get_table_columns_from_postgres(POSTGRES_CONN_ID, list(TABLES_DB1.keys()))
 default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=2)
@@ -24,7 +26,7 @@ default_args = {
 
 
 with DAG(
-    "olist_db1_ingestion_dag",
+    "olist_db1_ingestion_dag_ayahany",
     default_args=default_args,
     start_date=datetime(2025, 8, 22),
     end_date=datetime(2025, 8, 28),
@@ -34,6 +36,10 @@ with DAG(
 
 ) as dag:
     for tbl, pk in TABLES_DB1.items():
+        
+        columns = schema[tbl]
+        merge_sql = generate_merge_sql(tbl, pk, columns, BIGQUERY_DATASET)
+
         export = PostgresToGCSOperator(
             task_id=f"{tbl}_export_to_gcs",
             postgres_conn_id=POSTGRES_CONN_ID,
@@ -63,13 +69,7 @@ with DAG(
             task_id=f"{tbl}_merge_to_main",
             configuration={
                 "query": {
-                    "query": f"""
-                        MERGE `{BIGQUERY_DATASET}.{tbl}_ayahany` T
-                        USING `{BIGQUERY_DATASET}.{tbl}_staging_ayahany` S
-                        ON T.{pk} = S.{pk}
-                        WHEN MATCHED THEN UPDATE SET *
-                        WHEN NOT MATCHED THEN INSERT ROW
-                    """,
+                    "query": merge_sql,
                     "useLegacySql": False,
                 }
             },
