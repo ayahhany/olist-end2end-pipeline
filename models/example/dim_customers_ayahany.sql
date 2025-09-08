67e48d3f72fdd5aea10a0b54320c1896f66923a0
Aya Hany <ayahhany33@gmail.com>
2025-09-08 23:27:52 +0000

    deleting examples directory

--- models/example/dim_customers_ayahany.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='incremental',
-    incremental_strategy = 'merge',
-    unique_key = 'customer_id'
-    ) }}
-
-SELECT 
-customer_id, customer_unique_id, customer_city, customer_state 
-from project_landing.customers_ayahany
