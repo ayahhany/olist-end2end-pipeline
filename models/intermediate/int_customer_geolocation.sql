202b590b4e1d007db1dc49f679835f9c47474d8f
Aya Hany <ayahhany33@gmail.com>
2025-09-09 00:02:32 +0000

    int_customer_geolocation.sql created

---
+++ models/intermediate/int_customer_geolocation.sql
@@ -0,0 +1,31 @@
+{{ config(
+    materialized='view'
+) }}
+
+WITH customers AS (
+    SELECT * FROM {{ ref('stg_customers_ayahany') }}
+),
+
+geolocation AS (
+    SELECT * FROM {{ ref('stg_geolocation_ayahany') }}
+),
+
+final AS (
+    SELECT
+        c.customer_id,
+        c.customer_unique_id,
+        c.customer_zip_code_prefix,
+        c.customer_city,
+        c.customer_state,
+        g.latitude,
+        g.longitude,
+        g.city,
+        g.state,
+        c.updated_at_timestamp
+
+    FROM customers c
+    LEFT JOIN geolocation g
+        ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
+)
+
+SELECT * FROM final
