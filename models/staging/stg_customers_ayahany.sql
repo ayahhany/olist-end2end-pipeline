a7450e31fbfae359591c1febd8cf8541ee401e51
Aya Hany <ayahhany33@gmail.com>
2025-09-10 10:48:43 +0000

    stg_customers_ayahany.sql modified

--- models/staging/stg_customers_ayahany.sql
+++ models/staging/stg_customers_ayahany.sql
@@ -1,22 +1,5 @@
 {{ config(materialized='view') }}
 
-<<<<<<< HEAD
-with source as ( 
-    select * from {{ source('project_landing', 'customers_ayahany') }} 
-), 
-
-renamed as ( 
-    select 
-        customer_id, 
-        customer_unique_id, 
-        customer_zip_code_prefix, 
-        customer_city, 
-        customer_state 
-    from source 
-) 
-
-select * from renamed; 
-=======
 SELECT 
     customer_id, 
     customer_unique_id, 
@@ -26,8 +9,4 @@ SELECT
     cast(updated_at_timestamp as timestamp) as updated_at_timestamp 
 FROM 
     project_landing.customers_ayahany
-
-
->>>>>>> b905eb28d4708165c553b19e025c4ac1a67343fd
-
  
